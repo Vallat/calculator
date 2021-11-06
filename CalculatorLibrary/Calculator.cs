25c9074e969df9613eaccf10b8d208a52e338fac
vallat <vox.crit2013@gmail.com>
2021-11-06 22:04:19 +0400

    Basic operations with 2 numbers and console interface

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorLibrary;
+
+public class Calculator
+{
+	public decimal do_sum(decimal first_num, decimal second_num)
+	{
+		return first_num + second_num;
+	}
+
+	public decimal do_multiplication(decimal first_num, decimal second_num)
+	{
+		return first_num * second_num;
+	}
+
+	public decimal do_division(decimal first_num, decimal second_num)
+	{
+		return first_num / second_num;
+	}
+}
---
+++ ConsoleInterface/ConsoleInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CalculatorLibrary;
+
+namespace ConsoleInterface
+{
+	class ConsoleInterface
+	{
+		static void Main()
+		{
+			Calculator calculator = new Calculator();
+			Console.WriteLine("Enter 2 numbers. Available operations: +, -, *, /");
+			string? input = Console.ReadLine();
+			if(input is null)
+				return;
+			input = input.Replace(".", ",");
+			string[] strings = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+			decimal result = strings[1][0] switch 
+			{
+				'+' => calculator.do_sum(Convert.ToDecimal(strings[0]), Convert.ToDecimal(strings[2])),
+				'-' => calculator.do_sum(Convert.ToDecimal(strings[0]), -Convert.ToDecimal(strings[2])),
+				'*' => calculator.do_multiplication(Convert.ToDecimal(strings[0]), Convert.ToDecimal(strings[2])),
+				'/' => calculator.do_division(Convert.ToDecimal(strings[0]), Convert.ToDecimal(strings[2])),
+				_ => 0
+			};
+			Console.WriteLine($"{strings[0]} {strings[1]} {strings[2]} = {result}");
+		}
+	}
+}
